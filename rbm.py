34270b725625fbf4b42758bf37cb81eace325323
Ty Brennan <tyabrennan@gmail.com>
2023-03-17 10:30:26 -0700

    brain tutorials

--- rbm.py
+++ rbm.py
@@ -2,11 +2,10 @@ import numpy as np
 import hopfield as h
 
 def main():
-    state = np.random.rand(100)
-    state = np.array([i > 0.5 for i in state], dtype = np.short)
-
+    np.random.seed(0)
+    v = np.random.rand(100)
+    v = np.array([i > 0.5 for i in v], dtype = np.short)
     
-    # assert state.min() == 0 and state.max() == 1, state
 
 if __name__ == '__main__':
     main()
