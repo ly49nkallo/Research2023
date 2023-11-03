0dc4292bb32e055e800183394d1d8dea358ae3c5
ly49nkallo <tyabrennan@gmail.com>
2023-11-02 23:45:06 -0700

    hopf euler

--- Research2/model_num.py
+++ Research2/model_num.py
@@ -64,31 +64,34 @@ def derivative(x: np.ndarray, p: np.ndarray) -> np.ndarray:
 
     return np.array([dxhb, dxa, dpm, dpgs, dpT])
 
-xhist = []
-x0 = -1, 0, .5, .5, .5
-import json
-from tqdm import tqdm
-with open('./model_values.json', 'r') as f:
-    p = json.load(f)
-    params = tuple(p.values())
-    params = np.array(params)
-    params[11] = 0.6
+if __name__ == '__main__':
+    xhist = []
+    x0 = -1, 0, .5, .5, .5
+    import json
+    from tqdm import tqdm
+    import os
+
+    with open(os.path.dirname(__file__) + os.sep  + './model_values.json', 'r') as f:
+        p = json.load(f)
+        params = tuple(p.values())
+        params = np.array(params)
+        params[11] = 0.6
 
 
-x = np.array(x0)
-dt = 1e-2
-steps = 1e6
-params[8] = 900
+    x = np.array(x0)
+    dt = 1e-2
+    steps = 1e6
+    params[8] = 900
 
-steps_r = 1 / steps * 900
+    steps_r = 1 / steps * 900
 
-for i in tqdm(range(int(steps))):
-    ret = derivative(x, params)
-    params[8] -= steps_r
-    x += ret * dt
-    xhist.append(x[0])
+    for i in tqdm(range(int(steps))):
+        ret = derivative(x, params)
+        params[8] -= steps_r
+        x += ret * dt
+        xhist.append(x[0])
 
-plt.figure()
-plt.plot(xhist[1000:])
-plt.show()
+    plt.figure()
+    plt.plot(xhist[1000:])
+    plt.show()
 
